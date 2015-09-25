1147f12d4099ff69fe8d5ffb57bb9948a5911c18
Tim Shedor <tim@wearefine.com>
2015-09-25 15:30:59 -0700

    better documentation inline with JSDoc 3 standards

--- source/javascripts/frob_core_helpers.js
+++ source/javascripts/frob_core_helpers.js
@@ -2,18 +2,25 @@
 
 'use strict';
 
+/**
+ * Frob core helpers
+ * @namespace
+ */
 var FCH = {
 
   /* Listener Arrays */
+  /** @type {Array.<function>} */
   resize: [],
+  /** @type {Array.<function>} */
   scroll: [],
+  /** @type {Array.<function>} */
   ready: [],
+  /** @type {Array.<function>} */
   load: [],
 
-
   /**
-  * @function init Start everything up
-  **/
+   * @description Start everything up
+   */
   init: function() {
 
     // IE detection
@@ -35,38 +42,40 @@ var FCH = {
   },
 
   /**
-  * @function setDefault Apply value to variable if it has none
-  * @param {var} variable - variable to set default to
-  * @param {anything} value - default value to attribute to variable
-  */
+   * @description Apply value to variable if it has none
+   * @param {var} variable - variable to set default to
+   * @param {*} value - default value to attribute to variable
+   * @return {*} Existing value or passed value argument
+   */
   setDefault: function(variable, value){
     return (typeof variable === 'undefined') ? value : variable;
   },
 
   /**
-  * @function smoothScroll Nice, unjanky scroll to element
-  * @param {jQuery object} target - scroll to the top of this object
-  * @param {optional integer} duration {2000} - how long the scroll lasts
-  * @param {optional integer} delay {100} - how long to wait after trigger
-  * @param {optional integer} offset {0} - additional offset to add to the scrollTop
-  */
-  smoothScroll: function(target, duration, delay, offset){
+   * @description Nice, unjanky scroll to element
+   * @param {jQuery} $target - Scroll to the top of this object
+   * @param {Number} [duration=2000] - How long the scroll lasts
+   * @param {Number} [delay=100] - How long to wait after trigger
+   * @param {Number} [offset=0] - Additional offset to add to the scrollTop
+   */
+  smoothScroll: function($target, duration, delay, offset){
     duration = this.setDefault(duration, 2000);
     delay = this.setDefault(delay, 100);
     offset = this.setDefault(offset, 0);
 
     setTimeout(function(){
       $('html,body').animate({
-        scrollTop: target.offset().top + offset
+        scrollTop: $target.offset().top + offset
       }, duration);
     }, delay);
   },
 
   /**
-  * @function localStore Store a string locally
-  * @param {string} key - accessible identifier
-  * @param {string} obj - value of identifier
-  */
+   * @description Store a string locally
+   * @param {String} key - Accessible identifier
+   * @param {String} obj - Value of identifier
+   * @return {String} Value of key in localStorage
+   */
   localSet: function(key, obj) {
     var value = JSON.stringify(obj);
     localStorage[key] = JSON.stringify(obj);
@@ -75,9 +84,10 @@ var FCH = {
   },
 
   /**
-  * @function localRetrive Retrieve localstorage object value
-  * @param {string} key - accessible identifier
-  */
+   * @description Retrieve localstorage object value
+   * @param {String} key - Accessible identifier
+   * @return {String|Boolean} Value of localStorage object or false if key is undefined
+   */
   localGet: function(key) {
     if (typeof localStorage[key] !== 'undefined') {
       return JSON.parse(localStorage[key]);
@@ -87,44 +97,63 @@ var FCH = {
   },
 
   /**
-  * @function localClear Clear value of localstorage object. If no key is passed, clear all objects
-  * @param {optional string} key - accessible identifier
-  */
+   * @description Clear value of localstorage object. If no key is passed, clear all objects
+   * @param {String} [key] - Accessible identifier
+   * @return {Undefined} Result of clear or removeItem action
+   */
   localClear: function(key){
     return typeof key === 'undefined' ? localStorage.clear() : localStorage.removeItem(key);
   },
 
   /**
-  * @function ieIE check if IE is current browser
-  * @param {integer} version
-  */
+   * @description Check if IE is current browser
+   * @param {Number|String} version
+   * @return {Boolean}
+   */
   isIE: function(version) {
     var regex = new RegExp('msie' + (!isNaN(version)?('\\s' + version) : ''), 'i');
     return regex.test(navigator.userAgent);
   },
 
   /**
-  * @function exists check for existence of element on page
-  * @param {object} query - JavaScript object
-  */
+   * @description Check for existence of element on page
+   * @param {String} query - JavaScript object
+   * @return {Boolean}
+   */
   exists: function(query) {
     return !!document.querySelector(query);
   },
 
   /**
-  * @function breakpoints Provides accessible booleans for fluctuating screensizes
-  */
+   * @description Provides accessible booleans for fluctuating screensizes
+   */
   breakpoints: function() {
     var ww, wh;
 
     ww = window.innerWidth;
     wh = window.innerHeight;
-
+    /**
+     * Dimensions
+     * @namespace
+     * @description Holder for screen size numbers, set on load and reset on resize
+     * @property {Number} ww - Window width
+     * @property {Number} wh - Window height
+     */
     FCH.dimensions = {
       ww: ww,
       wh: wh
     };
 
+    /**
+     * Breakpoints
+     * @namespace
+     * @description Holder for responsive breakpoints, set on load and reset on resize
+     * @property {Boolean} small - Window width is less than 768
+     * @property {Boolean} small_up - Window width is greater than 767
+     * @property {Boolean} medium_portrait - Window width is between 767 and 960
+     * @property {Boolean} medium - Window width is between 767 and 1025
+     * @property {Boolean} large - Window width is greater than 1024
+     */
     FCH.bp = {
       small: ww < 768,
       small_up: ww > 767,
@@ -135,21 +164,22 @@ var FCH = {
   },
 
   /**
-  * @function hasClass Determine if element has class with vanilla JS
-  * @param {object} el
-  * @param {string} cls
-  * @source http://jaketrent.com/post/addremove-classes-raw-javascript/
-  */
+   * @description Determine if element has class with vanilla JS
+   * @param {Object} el JavaScript element
+   * @param {String} cls Class name
+   * @see {@link http://jaketrent.com/post/addremove-classes-raw-javascript/}
+   * @return {Boolean}
+   */
   hasClass: function(el, cls) {
     return !!el.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
   },
 
   /**
-  * @function addClass Add class to element with vanilla JS
-  * @param {object} el
-  * @param {string} cls
-  * @source http://jaketrent.com/post/addremove-classes-raw-javascript/
-  */
+   * @description Add class to element with vanilla JS
+   * @param {Object} el JavaScript element
+   * @param {String} cls Class name
+   * @see {@link http://jaketrent.com/post/addremove-classes-raw-javascript/}
+   */
   addClass: function(el, cls) {
     if (!this.hasClass(el, cls)) {
       el.className = el.className.trim();
@@ -158,11 +188,11 @@ var FCH = {
   },
 
   /**
-  * @function removeClass Remove class from element with vanilla JS
-  * @param {object} el
-  * @param {string} cls
-  * @source http://jaketrent.com/post/addremove-classes-raw-javascript/
-  */
+   * @description Remove class from element with vanilla JS
+   * @param {Object} el JavaScript element
+   * @param {String} cls Class name
+   * @see {@link http://jaketrent.com/post/addremove-classes-raw-javascript/}
+   */
   removeClass: function(el, cls) {
     if (this.hasClass(el, cls)) {
       var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
@@ -171,9 +201,9 @@ var FCH = {
   },
 
   /**
-  * @function Increase screen performance and frames per second by diasbling pointer events on scroll
-  * @source http://www.thecssninja.com/css/pointer-events-60fps
-  */
+   * @description Increase screen performance and frames per second by diasbling pointer events on scroll
+   * @see {@link http://www.thecssninja.com/css/pointer-events-60fps}
+   */
   mobileFPS: function(){
     var scroll_timer,
         body = document.getElementsByTagName('body')[0],
@@ -192,12 +222,11 @@ var FCH = {
   },
 
 
-  // Private
-
   /**
-  * @function _attachChildListeners Attach hooks on child objects to the listener arrays
-  * @param {string} listener
-  */
+   * @protected
+   * @description Attach hooks on child objects to the listener arrays
+   * @param {String} listener - Such as 'resize' or 'load'
+   */
   _attachChildListeners: function(listener) {
     var kids = Object.keys(FC);
 
@@ -209,14 +238,13 @@ var FCH = {
         FCH[listener].push( child_func );
       }
     }
-
   },
 
   /**
-  * @function _callListener Execute listeners using the bundled arrays
-  * @param {string} listener What to hear for, i.e. scroll, resize
-  * @implied_param FC must be defined
-  */
+   * @protected
+   * @description Execute listeners using the bundled arrays
+   * @param {String} listener - What to hear for, i.e. scroll, resize
+   */
   _callListener: function(listener) {
     var listener_array = this[listener];
 
@@ -227,12 +255,13 @@ var FCH = {
   },
 
   /**
-  * @function _throttle fire events more efficiently
-  * @param {string} type Listener function to trump
-  * @param {string} name New name for listener
-  * @param {Object} obj Object to bind/watch (defaults to window)
-  * @source https://developer.mozilla.org/en-US/docs/Web/Events/scroll
-  */
+   * @protected
+   * @description Fire events more efficiently
+   * @param {String} type - Listener function to trump
+   * @param {String} name - New name for listener
+   * @param {Object} obj - Object to bind/watch (defaults to window)
+   * @see {@link https://developer.mozilla.org/en-US/docs/Web/Events/scroll}
+   */
   _throttle: function(type, name, obj) {
     obj = obj || window;
     var running = false;
@@ -251,8 +280,9 @@ var FCH = {
   },
 
   /**
-  * @function _attachListeners actually bind the listeners to objects
-  */
+   * @protected
+   * @description Actually bind the listeners to objects
+   */
   _attachListeners: function() {
     var _this = this;
     var listener = 'optimized';
@@ -283,7 +313,10 @@ var FCH = {
 
 /* Cached jQuery variables */
 if(typeof jQuery !== 'undefined') {
+  /** @type {jQuery} */
   FCH.$body = jQuery('body');
+  /** @type {jQuery} */
   FCH.$window = jQuery(window);
+  /** @type {jQuery} */
   FCH.$document = jQuery(document);
 }
